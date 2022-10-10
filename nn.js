e8e052dd198b8d1a24f2e1854fc2f6718666ce0b
Matthew <matthewg_@hotmail.com>
2022-10-10 11:58:31 -0500

    Initial commit

---
+++ nn.js
@@ -0,0 +1,12 @@
+//neural network js
+
+var num = require('numjs');
+var arr = num.array([2,1,2]);
+
+exports.printArr = function() {
+    var thing = arr[0]
+    
+    return ""+arr.T;
+}
+
+//console.log(arr);
